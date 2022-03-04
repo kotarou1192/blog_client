012b770c1a0af6022334e1a4de436909e040f607
kousuke ida <takashiii1192@gmail.com>
2022-03-04 09:35:02 +0000

    add: logo icon

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,1 +1,3 @@
 export const sitekey = "6LfbaiscAAAAAE1AUTOJKd7GY-aoVrfTc7E8AIEC";
+export const iconUrl = "https://takashiii-hq.com/site_icon.png";
+export const headerLogoURL = "https://takashiii-hq.com/header_logo.png";
