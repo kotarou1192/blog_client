f37c95794f2d2886ebce2d5c13d83d3173f1705a
kousuke ida <takashiii1192@gmail.com>
2022-03-08 11:49:32 +0000

    update: icon url

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,6 +1,6 @@
 export const sitekey = "6LfbaiscAAAAAE1AUTOJKd7GY-aoVrfTc7E8AIEC";
-export const iconUrl = "https://takashiii-hq.com/site_icon.png";
-export const headerLogoURL = "https://takashiii-hq.com/header_logo.png";
-export const smileReo = "https://takashiii-hq.com/smile_reo.png";
-export const sadReo = "https://takashiii-hq.com/sad_reo.png";
-export const ohReo = "https://takashiii-hq.com/oh_reo.png";
+export const iconUrl = "https://blog-md.net/site_icon.png";
+export const headerLogoURL = "https://blog-md.net/header_logo.png";
+export const smileReo = "https://blog-md.net/smile_reo.png";
+export const sadReo = "https://blog-md.net/sad_reo.png";
+export const ohReo = "https://blog-md.net/oh_reo.png";
